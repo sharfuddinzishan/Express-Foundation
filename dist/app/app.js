d1d943270931e790b7dd9953bc5da8eb26b9892a
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-22 02:23:31 +0600

    Get Post Query Param

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,7 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// Parser
+app.use(express_1.default.json());
+app.use(express_1.default.text());
 app.get('/', (req, res) => {
     res.send('Hello World Zishan!');
+});
+app.post('/:id/:semester', (req, res) => {
+    let { id, semester } = req.params || {};
+    res.send(`Got Param Data ${id},${semester}`);
+    console.log(req.params);
+});
+app.post('/', (req, res) => {
+    let { id, semester } = req.query || {};
+    res.send(`Got Query Data ${id},${semester}`);
+    console.log(req.query);
+});
+app.post('/poem', (req, res) => {
+    console.log(req.body);
+    res.send('Poem Send!');
+});
+app.post('/getData', (req, res) => {
+    console.log(req.body);
+    res.send('got Data');
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,35 @@
-import express from 'express'
+import express, { Request,Response } from 'express'
 const app = express()
 
-app.get('/', (req, res) => {
+// Parser
+app.use(express.json())
+app.use(express.text())
+
+app.get('/', (req:Request, res:Response) => {
   res.send('Hello World Zishan!')
+})
+
+app.post('/:id/:semester', (req:Request, res:Response) => {
+  let {id,semester}=req.params||{}
+  res.send(`Got Param Data ${id},${semester}`)
+  console.log(req.params);
+})
+
+app.post('/', (req:Request, res:Response) => {
+  let {id,semester}=req.query||{}
+  res.send(`Got Query Data ${id},${semester}`)
+  console.log(req.query);
+})
+
+
+app.post('/poem', (req:Request, res:Response) => {
+  console.log(req.body);
+  res.send('Poem Send!')
+})
+
+app.post('/getData',(req:Request,res:Response)=>{
+  console.log(req.body);
+  res.send('got Data')
 })
 
 export default app;
