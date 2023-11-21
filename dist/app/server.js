e93c77233e1b944c54790cef41cef43aefc95dfa
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-21 20:53:40 +0600

    setup

--- dist/app/server.js
+++ dist/app/server.js
@@ -14,7 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 let server;
-const port = 3000;
+const port = 4000;
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
         server = app_1.default.listen(port, () => {
--- src/app/server.ts
+++ src/app/server.ts
@@ -2,7 +2,7 @@ import app from './app'
 import {Server} from 'http'
 
 let server:Server
-const port = 3000
+const port = 4000
 
 async function bootstrap(){
   server=app.listen(port, () => {
